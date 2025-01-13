3426501694f94c1b5dabcbce916cfccb676ade2a
haarishnair <haarishnair@gmail.com>
2025-01-14 03:25:06 +0800

    Updated Login Page

--- src/components/LoginComponent.jsx
+++ src/components/LoginComponent.jsx
@@ -1,90 +1,20 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import styles from "../styles/LoginRegistration.module.css";
 
-import styles from "../styles/LoginComponent.module.css"; 
-import microsoftLogo from "../../public/img/Microsoft.png";
-import pebbleLogo from "../../public/img/pebbleLongLogo.png";
-
-
-const LoginComponent = () => {
-
-  const pebbleLogo = "img/pebbleLogo.png";
-  const microsoftLogo = "icons/microsoft.png"
-
-
-  const handleMicrosoftLogin = () => {
-    console.log("Microsoft Login clicked");
-  };
-
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-    const email = e.target.email.value;
-    const password = e.target.password.value;
-    console.log("Email:", email, "Password:", password);
-  };
-
+function LoginComponent(props) {
   return (
     <div className={styles.loginComponent}>
-      <div className={styles.pebbleLogo}>
-        <img
-          src={pebbleLogo}
-          alt="Pebble Logo"
-          className={styles.pebbleLogo}
-        />
-      </div>
-      <div className={styles.loginText}>
-        <h1>Log in using your account on:</h1>
-      </div>
-      <div className={styles.microsoftLogin}>
-        <button onClick={handleMicrosoftLogin} className={styles.microsoftButton}>
-          <img
-            src={microsoftLogo}
-            alt="Microsoft Logo"
-            className={styles.microsoftLogo}
-          />
-          Login with Microsoft
-        </button>
-      </div>
-      <div className={styles.pebbleLoginText}>
-        <h1>Login with your Pebble account</h1>
-      </div>
-      <div className={styles.otherLogin}>
-        <form onSubmit={handleFormSubmit}>
-          <input
-            type="text"
-            name="username"
-            placeholder="Username"
-            required
-            className={styles.inputField}
-          />
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            required
-            className={styles.inputField}
-          />
-          <div className={styles.passwordCheck}>
-            <div className={styles.forgotPassword}>
-                <p>Forgot Password?</p>
-            </div>
-            <div className={styles.rememberPassword}>
-                  <input
-                    type="checkbox"
-                    name="rememberPassword"
-                    className={styles.checkbox}
-                />
-                <p>Remember me</p>
-            </div>
-          </div>
-          <button type="submit" className={styles.loginButton}>Log In</button>
-        </form>
-        <div className={styles.createAccount}>
-          <p>Don't have an account? <Link to="/register"><span className={styles.register}>Register here</span></Link></p>
-        </div>
-      </div>
+      <input
+        type={props.type}
+        name={props.name}
+        id={props.name}
+        value={props.value}
+        onChange={props.onChange}
+        className={`${styles.inputField}`}
+        placeholder={props.label} // Placeholder for fields
+      />
     </div>
   );
-};
+}
 
 export default LoginComponent;
--- src/containers/LoginFormContainer.jsx
+++ src/containers/LoginFormContainer.jsx
@@ -1,21 +1,106 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import LoginComponent from "../components/LoginComponent";
-import styles from "../styles/LoginFormContainer.module.css"; 
-import campusImage from "../../public/img/campus.png"; 
+import styles from "../styles/LoginRegistration.module.css";
 
 const LoginFormContainer = () => {
+
+  const handleMicrosoftLogin = () => {
+    console.log("Microsoft Login clicked");
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log("Email:", email, "Password:", password);
+  };
+
+  
+  // Defining all the fields
+  const formFields = {
+      email : {
+          label : "Email Address",
+          type : "text",
+      },
+      password : {
+          label : "Password",
+          type : "password",
+      }
+  };
+
+  // Factory to make default object value for state hooks
+  function generateDefaultObject(defaultValue) {
+      const newObject = { ...formFields };
+      for (const key in newObject) newObject[key] = defaultValue;
+
+      return newObject;
+  }
+
+
+  // State hooks
+  const [formData, setFormData] = useState(generateDefaultObject(""));
+
+  // Effect hooks
+  useEffect(() => {
+      console.log(formData);
+    }, [formData]);
+
+  
+  // Updates state object on each keypress
+  const handleInputChange = (e) => {
+      const {name, value} = e.target;
+
+      setFormData((prev) => ({
+          ...prev,
+          [name] : value
+      }));
+  }
+  
+
   return (
     <div className={styles.loginContainer}>
-      <div className={styles.background}>
-        <img
-          src={campusImage}
-          alt="Background"
-          className={styles.backgroundImg} 
-        />
+      <div className={styles.microsoftLogin}>
+        <button onClick={handleMicrosoftLogin} className={styles.microsoftButton}>
+          <img
+            src="img/microsoft.png"
+            alt="Microsoft Logo"
+            className={styles.microsoftLogo}
+          />
+          Login with Microsoft
+        </button>
       </div>
-      <div className={styles.loginForm}>
-        <LoginComponent />
+      <div className={styles.pebbleLoginText}>
+        <h1>Login with your Pebble account</h1>
       </div>
+      <form onSubmit={handleSubmit}>
+
+        {
+          Object.entries(formFields).map(([key, value]) => (
+            <LoginComponent 
+              name={key}
+              type={value.type}
+              value={formData[key]}
+              label={value.label}
+              onChange={handleInputChange}
+            />
+          ))
+        }
+        <div className={styles.passwordCheck}>
+          <div className={styles.forgotPassword}>
+            <p>Forgot Password?</p>
+          </div>
+          <div className={styles.rememberPassword}>
+            <input
+              type="checkbox"
+              name="rememberPassword"
+              className={styles.checkbox}
+            />
+            <p>Remember me</p>
+          </div>
+        </div>
+        <button type="submit" className={styles.loginButton}>Log In</button>
+
+      </form>
     </div>
   );
 };
