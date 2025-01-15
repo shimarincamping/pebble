57bfe4f97bbbcb90a56f95f2dd4aea7d497bea11
Anoop Singh <anoopsinghhs77@gmail.com>
2025-01-15 18:56:39 +0800

    Changed locations of images from src to public

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -1,26 +1,23 @@
 import React from "react";
 import styles from "../styles/PageHeader.module.css"; 
-import HamburgerIcon from '../assets/Menu.svg';
-import PebbleLongLogo from '../assets/pebbleLongLogo.png';
-import SearchIcon from '../assets/SearchIconForTopBar.svg';
 
 function PageHeader({handleLogoClick,handleMenuClick,handleSearch}){
     return(
         <>
             <div className={styles.HeaderContainerDiv}>
                 <div className={styles.HamburgerIconContainer}>
-                    <img className={styles.HamburgerIcon} src={HamburgerIcon} onclick={handleMenuClick} alt=""/>
+                    <img className={styles.HamburgerIcon} src="img/Menu.svg" onclick={handleMenuClick} alt=""/>
                 </div>
 
                 <div className={styles.PebbleLongLogoContainer} >  
-                    <img  className={styles.PebbleLongLogo} src={PebbleLongLogo} onclick={handleLogoClick} alt=""/> 
+                    <img  className={styles.PebbleLongLogo} src="img/pebbleLongLogo.png" onclick={handleLogoClick} alt=""/> 
                 </div>
                
                 <div className={styles.SearchBarContainer}>
                     <form className={styles.SearchForm}>
                         <input className={styles.SearchBarInput} placeholder="Search on PEBBLE..." />
                     </form>
-                    <img className={styles.SearchIcon} src={SearchIcon}  onclick={handleSearch} alt=""/>
+                    <img className={styles.SearchIcon} src="img/SearchIconForTopBar.svg"  onclick={handleSearch} alt=""/>
                 </div>
             </div>
         
