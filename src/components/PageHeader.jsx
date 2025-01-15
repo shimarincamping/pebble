d6bffa22f00c92d71361fc59fc671f541051ee80
Anoop Singh <anoopsinghhs77@gmail.com>
2025-01-15 17:30:49 +0800

    completed PageHeader.jsx

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -4,20 +4,23 @@ import HamburgerIcon from '../assets/Menu.svg';
 import PebbleLongLogo from '../assets/pebbleLongLogo.png';
 import SearchIcon from '../assets/SearchIconForTopBar.svg';
 
-function PageHeader(){
-    
+function PageHeader({handleLogoClick,handleMenuClick,handleSearch}){
     return(
         <>
             <div className={styles.HeaderContainerDiv}>
-                <img className={styles.HamburgerIcon} src={HamburgerIcon}/>
+                <div className={styles.HamburgerIconContainer}>
+                    <img className={styles.HamburgerIcon} src={HamburgerIcon} onclick={handleMenuClick} alt=""/>
+                </div>
 
                 <div className={styles.PebbleLongLogoContainer} >  
-                    <img  className={styles.PebbleLongLogo} src={PebbleLongLogo}/> 
+                    <img  className={styles.PebbleLongLogo} src={PebbleLongLogo} onclick={handleLogoClick} alt=""/> 
                 </div>
                
                 <div className={styles.SearchBarContainer}>
-                    <form>Search on PEBBLE..</form>
-                    <img className={styles.SearchIcon} src={SearchIcon}/>
+                    <form className={styles.SearchForm}>
+                        <input className={styles.SearchBarInput} placeholder="Search on PEBBLE..." />
+                    </form>
+                    <img className={styles.SearchIcon} src={SearchIcon}  onclick={handleSearch} alt=""/>
                 </div>
             </div>
         
