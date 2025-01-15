3b83eef7bcff42be6552eb666fae2a44a18d3f41
Brendan Ooi <brendanooi16@gmail.com>
2025-01-16 02:12:15 +0800

    improved forum header and thread UI

--- src/components/Forum/ForumPageHeader.jsx
+++ src/components/Forum/ForumPageHeader.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { AiOutlinePlus } from "react-icons/ai";
+import styles from "../../styles/LearnForum.module.css";
 
 export default function ForumPageHeader() {
   return (
-    <section>
+    <section className={styles.forumHeader__header}>
       <Link to="#">
-        <button>
+        <button className={styles.forumHeader__button}>
           <AiOutlinePlus size={20} /> Create New Thread
         </button>
       </Link>
