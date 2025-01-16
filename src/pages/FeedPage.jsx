ccf1732bdf70669a2c1997d7f971bc747c3fc679
Najihah Syahriza <najihahsyahriza@gmail.com>
2025-01-16 17:18:48 +0800

    Resolved merge conflict in FeedPage.jsx

--- src/pages/FeedPage.jsx
+++ src/pages/FeedPage.jsx
@@ -3,6 +3,42 @@ import ApplicationSidebar from "../containers/ApplicationSidebar";
 import ApplicationMainContent from "../containers/ApplicationMainContent";
 
 import ProfileSidebarCardContainer from "../containers/ProfileSidebarCardContainer";
+import DashboardStatsSidebarCardContainer from "../containers/DashboardStatsSidebarCardContainer";
+import NetworkSidebarCardContainer from "../containers/NetworkSidebarCardContainer";
+
+
+import styles from "../styles/global.module.css";
+
+
+function FeedPage() {
+  return (
+    <div
+      className={`${styles.mainApplicationGridContainer} ${styles.mainApplicationNarrowBody}`}  
+    >
+      <ApplicationSidebar>
+        <ProfileSidebarCardContainer />
+      </ApplicationSidebar>
+
+      <ApplicationMainContent>
+      </ApplicationMainContent>
+
+      <ApplicationSidebar>
+        <DashboardStatsSidebarCardContainer />
+        <NetworkSidebarCardContainer />
+      </ApplicationSidebar>
+
+    </div>
+  );
+}
+
+export default FeedPage;
+
+import React from "react";
+import ApplicationSidebar from "../containers/ApplicationSidebar";
+import ApplicationMainContent from "../containers/ApplicationMainContent";
+
+import ProfileSidebarCardContainer from "../containers/ProfileSidebarCardContainer";
+import PostCreationCard from "../components/PostCreationCard";
 
 import styles from "../styles/global.module.css";
 
@@ -20,10 +56,10 @@ function FeedPage() {
         <PostCreationCard />
       </ApplicationMainContent>
 
+      {
       <ApplicationSidebar>
-        <DashboardStatsSidebarCardContainer />
-        <NetworkSidebarCardContainer />
       </ApplicationSidebar>
+      }
 
     </div>
   );
