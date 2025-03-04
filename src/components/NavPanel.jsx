d50fb8409dc1b00c8e5a1063bed7cf3d9a92505e
Brendan Ooi <brendanooi16@gmail.com>
2025-03-04 21:16:00 +0800

    changed nav panel /roadmaps to /roadmap

--- src/components/NavPanel.jsx
+++ src/components/NavPanel.jsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import styles from '../styles/NavPanel.module.css';
+import React from "react";
+import styles from "../styles/NavPanel.module.css";
 
-function NavPanel({isVisible,HandleMenuClick,HandleBellClick,navigateTo}){
-
+function NavPanel({ isVisible, HandleMenuClick, HandleBellClick, navigateTo }) {
     // alert(props.isVisible);
     // let isVisible=-1;
 
-    return(
-        <div className={`${styles.NavPanelContainer} 
+    return (
+        <div
+            className={`${styles.NavPanelContainer} 
                          ${isVisible ? styles.visible : ""}
                         `}
-        > 
-            <div className={styles.Header}> 
-                <img 
-                    src="/img/TaylorsLogo.png" 
-                    className={styles.TaylorsLogo} 
+        >
+            <div className={styles.Header}>
+                <img
+                    src="/img/TaylorsLogo.png"
+                    className={styles.TaylorsLogo}
                     alt=""
                 />
 
-                <img 
-                    src="/img/WhiteMenuIcon.png" 
-                    className={styles.MenuIcon} 
+                <img
+                    src="/img/WhiteMenuIcon.png"
+                    className={styles.MenuIcon}
                     onClick={HandleMenuClick}
                     alt=""
                 />
@@ -29,11 +29,11 @@ function NavPanel({isVisible,HandleMenuClick,HandleBellClick,navigateTo}){
             <div className={styles.Body}>
                 <div onClick={navigateTo("/feed")}>
                     <div className={styles.ImgContainer} onClick="">
-                        <img src="/img/HomeIcon.png" alt=""/>
+                        <img src="/img/HomeIcon.png" alt="" />
                     </div>
                     <div className={styles.TextContainer}>
                         <h2>Home</h2>
-                    </div>   
+                    </div>
                 </div>
 
                 <div onClick={navigateTo("/rewards")}>
@@ -46,7 +46,7 @@ function NavPanel({isVisible,HandleMenuClick,HandleBellClick,navigateTo}){
                 </div>
 
                 <div onClick={navigateTo("/forum")}>
-                    <div className={styles.ImgContainer}> 
+                    <div className={styles.ImgContainer}>
                         <img src="/img/LearnIcon.png" alt=""></img>
                     </div>
                     <div className={styles.TextContainer}>
@@ -56,21 +56,20 @@ function NavPanel({isVisible,HandleMenuClick,HandleBellClick,navigateTo}){
 
                 <div onClick={navigateTo("/profile")}>
                     <div className={styles.ImgContainer}>
-                      <img src="/img/ProfileIcon.png" alt=""></img>
+                        <img src="/img/ProfileIcon.png" alt=""></img>
                     </div>
                     <div className={styles.TextContainer}>
                         <h2>Me</h2>
                     </div>
                 </div>
 
-                <div onClick={navigateTo("/roadmaps")}>
+                <div onClick={navigateTo("/roadmap")}>
                     <div className={styles.ImgContainer}>
                         <img src="/img/RoadMapIcon.png" alt=""></img>
                     </div>
                     <div className={styles.TextContainer}>
                         <h2>Roadmaps</h2>
                     </div>
-                    
                 </div>
 
                 <div onClick={navigateTo("/codingchallenge")}>
@@ -88,17 +87,15 @@ function NavPanel({isVisible,HandleMenuClick,HandleBellClick,navigateTo}){
                 <div className={styles.LogOut}>
                     <h3>LOG OUT</h3>
                 </div>
-                <img className={styles.BellIcon} 
-                     src="/img/BellIcon.png" 
-                     onClick={HandleBellClick}
-                     alt="" 
+                <img
+                    className={styles.BellIcon}
+                    src="/img/BellIcon.png"
+                    onClick={HandleBellClick}
+                    alt=""
                 />
-
             </div>
         </div>
     );
-
-
 }
 
-export default NavPanel; 
+export default NavPanel;
