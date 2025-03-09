c09fd39c38b274c7c5e73bf396513540a5e1454a
shimarincamping <93362779+shimarincamping@users.noreply.github.com>
2025-03-09 19:57:14 +0800

    Added useAuth for Profile, Goals, Coding Challenge Page

--- src/containers/GoalsPageContainer.jsx
+++ src/containers/GoalsPageContainer.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useAuth } from "../containers/AuthProvider";
 
 import RewardsPageHeader from '../components/RewardsPageHeader';
 import GoalSectionComponent from '../components/GoalSectionComponent';
@@ -6,10 +7,11 @@ import ComponentLoadingSpinner from '../components/ComponentLoadingSpinner';
 
 function GoalsPageContainer() {
 
+    const { user } = useAuth();
     const [goalsList, setGoalsList] = useState(null);
 
     const handleFetchData = async () => {
-        const currentUserID = "3oMAV7h8tmHVMR8Vpv9B" // Replace with value set by authentication feature, currently always Anoop
+        const currentUserID = await user;
 
         const fetchedData = await fetch(`${process.env.REACT_APP_API_URL}/goals`);
         const fetchedJsonData = await fetchedData.json();
