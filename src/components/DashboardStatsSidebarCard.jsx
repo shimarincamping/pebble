9848fe99308a973433bbd1f483a612e5ea72fd3b
Brendan Ooi <brendanooi16@gmail.com>
2025-03-12 18:53:25 +0800

    sidebar stats now has real data

--- src/components/DashboardStatsSidebarCard.jsx
+++ src/components/DashboardStatsSidebarCard.jsx
@@ -5,55 +5,65 @@ import { GiPlasticDuck } from "react-icons/gi";
 import styles from "../styles/Sidebar.module.css";
 
 export default function DashboardStatsSidebarCard(props) {
-  return (
-    <section>
-          <div className={`${styles.statsSidebarCard}`}>
-            <h2 className={`${styles.statsSidebarCard__title}`}>My Stats</h2>
-            <div
-              className={`${styles.statsSidebarCard__item} ${styles.statsSidebarCard__item__leaderboard}`}
-            >
-              <TbAward size={30} className={`${styles.statsSidebarCard__logo}`} />
-              <div className={`${styles.statsSidebarCard__item__desc}`}>
-                <span className={`${styles.statsSidebarCard__data}`}>
-                  #{props.leaderboardRank}&nbsp;
-                </span>
-                <span>&#40;{props.totalPoints} pts&#41;</span>
-              </div>
-            </div>
-            <div
-              className={`${styles.statsSidebarCard__item} ${styles.statsSidebarCard__item__points}`}
-            >
-              <TbCalendarMonth
-                size={30}
-                className={`${styles.statsCard__logo}`}
-              />
-              <div className={`${styles.statsSidebarCard__item__desc}`}>
-                <span className={`${styles.statsSidebarCard__data}`}>
-                  {props.monthlyPoints}pts&nbsp;
-                </span>
-                <span>this month</span>
-              </div>
-            </div>
-            <div>
-              <div
-                className={`${styles.statsSidebarCard__item} ${styles.statsSidebarCard__item__tickets}`}
-              >
-                <GiPlasticDuck
-                  size={30}
-                  className={`${styles.statsSidebarCard__logo}`}
-                />
-                <div className={`${styles.statsSidebarCard__item__desc}`}>
-                  <span className={`${styles.statsSidebarCard__data}`}>
-                    {props.tickets}&nbsp;
-                  </span>
-                  <span>tickets</span>
+    return (
+        <section>
+            <div className={`${styles.statsSidebarCard}`}>
+                <h2 className={`${styles.statsSidebarCard__title}`}>
+                    My Stats
+                </h2>
+                <div
+                    className={`${styles.statsSidebarCard__item} ${styles.statsSidebarCard__item__leaderboard}`}
+                >
+                    <TbAward
+                        size={30}
+                        className={`${styles.statsSidebarCard__logo}`}
+                    />
+                    <div className={`${styles.statsSidebarCard__item__desc}`}>
+                        <span className={`${styles.statsSidebarCard__data}`}>
+                            #{props.leaderboardRank}&nbsp;
+                        </span>
+                        <span>&#40;{props.totalPoints} pts&#41;</span>
+                    </div>
                 </div>
-                <div className={`${styles.statsSidebarCard__logo__gift}`}>
-                  <TbGift size={20} />
+                <div
+                    className={`${styles.statsSidebarCard__item} ${styles.statsSidebarCard__item__points}`}
+                >
+                    <TbCalendarMonth
+                        size={30}
+                        className={`${styles.statsCard__logo}`}
+                    />
+                    <div className={`${styles.statsSidebarCard__item__desc}`}>
+                        <span className={`${styles.statsSidebarCard__data}`}>
+                            {props.totalPoints}pts
+                        </span>
+                    </div>
                 </div>
-              </div>
+                <div>
+                    <div
+                        className={`${styles.statsSidebarCard__item} ${styles.statsSidebarCard__item__tickets}`}
+                    >
+                        <GiPlasticDuck
+                            size={30}
+                            className={`${styles.statsSidebarCard__logo}`}
+                        />
+                        <div
+                            className={`${styles.statsSidebarCard__item__desc}`}
+                        >
+                            <span
+                                className={`${styles.statsSidebarCard__data}`}
+                            >
+                                {props.tickets}&nbsp;
+                            </span>
+                            <span>tickets</span>
+                        </div>
+                        <div
+                            className={`${styles.statsSidebarCard__logo__gift}`}
+                        >
+                            <TbGift size={20} />
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-    </section>
-  );
+        </section>
+    );
 }
